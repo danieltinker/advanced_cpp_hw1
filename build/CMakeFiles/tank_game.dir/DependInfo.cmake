84d0f8711552fb4bb7ffa812872d68954bc2ec75
Daniel Baruch <danielb534@gmail.com>
2025-04-23 20:00:51 +0300

    make file

---
+++ build/CMakeFiles/tank_game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/danielbaruch/Documents/Git/advanced_cpp_hw1/Board.cpp" "CMakeFiles/tank_game.dir/Board.cpp.o" "gcc" "CMakeFiles/tank_game.dir/Board.cpp.o.d"
+  "/Users/danielbaruch/Documents/Git/advanced_cpp_hw1/GameState.cpp" "CMakeFiles/tank_game.dir/GameState.cpp.o" "gcc" "CMakeFiles/tank_game.dir/GameState.cpp.o.d"
+  "/Users/danielbaruch/Documents/Git/advanced_cpp_hw1/Tank.cpp" "CMakeFiles/tank_game.dir/Tank.cpp.o" "gcc" "CMakeFiles/tank_game.dir/Tank.cpp.o.d"
+  "/Users/danielbaruch/Documents/Git/advanced_cpp_hw1/TankAlgorithm.cpp" "CMakeFiles/tank_game.dir/TankAlgorithm.cpp.o" "gcc" "CMakeFiles/tank_game.dir/TankAlgorithm.cpp.o.d"
+  "/Users/danielbaruch/Documents/Git/advanced_cpp_hw1/main.cpp" "CMakeFiles/tank_game.dir/main.cpp.o" "gcc" "CMakeFiles/tank_game.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tank_game.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tank_game.dir/Board.cpp.o"
+  "CMakeFiles/tank_game.dir/Board.cpp.o.d"
+  "CMakeFiles/tank_game.dir/GameState.cpp.o"
+  "CMakeFiles/tank_game.dir/GameState.cpp.o.d"
+  "CMakeFiles/tank_game.dir/Tank.cpp.o"
+  "CMakeFiles/tank_game.dir/Tank.cpp.o.d"
+  "CMakeFiles/tank_game.dir/TankAlgorithm.cpp.o"
+  "CMakeFiles/tank_game.dir/TankAlgorithm.cpp.o.d"
+  "CMakeFiles/tank_game.dir/main.cpp.o"
+  "CMakeFiles/tank_game.dir/main.cpp.o.d"
+  "tank_game"
+  "tank_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tank_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
