94eb59a0d2fe7aba7de6b7a3f9e44635601ab40e
Daniel Baruch <danielb534@gmail.com>
2025-04-27 14:00:17 +0300

    parsing error handling log

--- Board.cpp
+++ Board.cpp
@@ -10,35 +10,100 @@ Board::Board(const std::string& filePath) {
     parseBoardFile(filePath);
 }
 
+
 void Board::parseBoardFile(const std::string& filePath) {
     std::ifstream file(filePath);
-    if (!file) throw std::runtime_error("Failed to open board file.");
+    if (!file) {
+        throw std::runtime_error("Failed to open board file: " + filePath);
+    }
 
-    file >> width >> height;
-    file.ignore();
+    int tempWidth, tempHeight;
+    if (!(file >> tempWidth >> tempHeight)) {
+        throw std::runtime_error("Invalid width/height declaration in board file.");
+    }
+    file.ignore();  // Ignore the rest of the line
+
+    width = tempWidth;
+    height = tempHeight;
     grid.resize(height, std::vector<Cell>(width));
 
-    file.clear();   
-    file.seekg(0, std::ios::beg); 
+    std::ofstream errorLog("input_errors.txt");
+    bool hasErrors = false;
+
+    int tank1Count = 0;
+    int tank2Count = 0;
     std::string line;
-    std::getline(file, line); // ignoring the first size defining line
-    // std::cout << line << std::endl;
-    for (int y = 0; y < height && std::getline(file, line); ++y) {
-        for (int x = 0; x < width && x < (int)line.size(); ++x) {
-            switch (line[x]) {
-                case '#': grid[y][x].content = CellContent::WALL; break;
-                case '@': grid[y][x].content = CellContent::MINE; break;
-                case '1': grid[y][x].content = CellContent::TANK1; break;
-                case '2': grid[y][x].content = CellContent::TANK2; break;
-                case ' ': grid[y][x].content = CellContent::EMPTY; break;
-                default:  grid[y][x].content = CellContent::EMPTY; break;
+
+    for (int y = 0; y < height; ++y) {
+        if (!std::getline(file, line)) {
+            hasErrors = true;
+            errorLog << "Warning: Missing row at " << y << ". Filling with EMPTY.\n";
+            continue;
+        }
+
+        for (int x = 0; x < width; ++x) {
+            char ch = (x < (int)line.size()) ? line[x] : ' ';
+
+            switch (ch) {
+                case '#':
+                    grid[y][x].content = CellContent::WALL;
+                    break;
+                case '@':
+                    grid[y][x].content = CellContent::MINE;
+                    break;
+                case '1':
+                    if (tank1Count == 0) {
+                        grid[y][x].content = CellContent::TANK1;
+                        tank1Count++;
+                    } else {
+                        grid[y][x].content = CellContent::EMPTY;
+                        hasErrors = true;
+                        errorLog << "Warning: Extra Tank 1 ignored at (" << x << "," << y << ").\n";
+                    }
+                    break;
+                case '2':
+                    if (tank2Count == 0) {
+                        grid[y][x].content = CellContent::TANK2;
+                        tank2Count++;
+                    } else {
+                        grid[y][x].content = CellContent::EMPTY;
+                        hasErrors = true;
+                        errorLog << "Warning: Extra Tank 2 ignored at (" << x << "," << y << ").\n";
+                    }
+                    break;
+                case ' ':
+                    grid[y][x].content = CellContent::EMPTY;
+                    break;
+                default:
+                    grid[y][x].content = CellContent::EMPTY;
+                    hasErrors = true;
+                    errorLog << "Warning: Unknown character '" << ch << "' treated as EMPTY at (" << x << "," << y << ").\n";
+                    break;
             }
         }
+
+        if ((int)line.size() > width) {
+            hasErrors = true;
+            errorLog << "Warning: Extra characters beyond declared width at row " << y << " ignored.\n";
+        }
+    }
+
+    // Skip extra rows
+    int skippedRows = 0;
+    while (std::getline(file, line)) {
+        skippedRows++;
+    }
+    if (skippedRows > 0) {
+        hasErrors = true;
+        errorLog << "Warning: " << skippedRows << " extra rows ignored beyond declared height.\n";
+    }
+
+    errorLog.close();
+
+    if (!hasErrors) {
+        std::remove("input_errors.txt");
     }
 }
-
-
-
 
 
 std::string Board::print(Direction dir1,Direction dir2) const {
